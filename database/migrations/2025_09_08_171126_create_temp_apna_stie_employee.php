4134cbcae2540fedf105b785eac88f65c6575adf
Digvijay Singh <dsrathore9549@gmail.com>
2025-09-09 00:03:25 +0530

    apna site temp flow make

---
+++ database/migrations/2025_09_08_171126_create_temp_apna_stie_employee.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('temp_apna_stie_employee', function (Blueprint $table) {
+            $table->id();
+            $table->string('employee_id',100)->unique();
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('temp_apna_stie_employee');
+    }
+};

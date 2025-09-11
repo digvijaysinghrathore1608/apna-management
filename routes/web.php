a40c5eb7bfe3873aa0700b0234a9758fe37fc3ad
Digvijay Singh <dsrathore9549@gmail.com>
2025-09-11 22:05:52 +0530

    add routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.index');
+})->name('dashboard');
+
+Route::get('/login', function () {
+    return view('auth.pages.index');
+})->name('login');
+Route::get('/register', function () {
+    return view('auth.pages.register');
+})->name('register');
+Route::get('/forgot-password', function () {
+    return view('auth.pages.forgot_password');
+})->name('forgot.password');

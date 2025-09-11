1a9ba77a817e890aa69e803059ea9209ac2d6ee2
Digvijay Singh <dsrathore9549@gmail.com>
2025-09-11 22:05:41 +0530

    create dashboard

---
+++ resources/views/partials/layouts/app.blade.php
@@ -0,0 +1,105 @@
+<!doctype html>
+
+<html lang="en" class="layout-menu-fixed layout-compact">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>Dashboard - Analytics | ApnaPayment</title>
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ assetOrDefault('assets/img/favicon/favicon.ico') }}" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
+
+    <link rel="stylesheet" href="{{ assetOrDefault('assets/vendor/fonts/iconify-icons.css') }}" />
+
+    <!-- Core CSS -->
+    <!-- build:css assets/vendor/css/theme.css  -->
+
+    <link rel="stylesheet" href="{{ assetOrDefault('assets/vendor/css/core.css') }}" />
+    <link rel="stylesheet" href="{{ assetOrDefault('assets/css/demo.css') }}" />
+
+    <!-- Vendors CSS -->
+
+    <link rel="stylesheet" href="{{ assetOrDefault('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+
+    <!-- endbuild -->
+
+    <link rel="stylesheet" href="{{ assetOrDefault('assets/vendor/libs/apex-charts/apex-charts.css') }}" />
+
+    <!-- Page CSS -->
+
+    <!-- Helpers -->
+    <script src="{{ assetOrDefault('assets/vendor/js/helpers.js') }}"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+
+    <script src="{{ assetOrDefault('assets/js/config.js') }}"></script>
+</head>
+
+<body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+            <!-- Menu -->
+            @include('partials.layouts.menu')
+            <!-- /Menu -->
+
+            <!-- Layout container -->
+            <div class="layout-page">
+                <!-- Navbar -->
+
+                @include('partials.layouts.nav')
+
+                <!-- / Navbar -->
+
+                @yield('content')
+
+            </div>
+            <!-- / Layout page -->
+        </div>
+
+        <!-- Overlay -->
+        <div class="layout-overlay layout-menu-toggle"></div>
+    </div>
+    <!-- / Layout wrapper -->
+
+    <!-- Core JS -->
+
+    <script src="{{ assetOrDefault('assets/vendor/libs/jquery/jquery.js') }}"></script>
+
+    <script src="{{ assetOrDefault('assets/vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{ assetOrDefault('assets/vendor/js/bootstrap.js') }}"></script>
+
+    <script src="{{ assetOrDefault('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+
+    <script src="{{ assetOrDefault('assets/vendor/js/menu.js') }}"></script>
+
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="{{ assetOrDefault('assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
+
+    <!-- Main JS -->
+
+    <script src="{{ assetOrDefault('assets/js/main.js') }}"></script>
+
+    <!-- Page JS -->
+    <script src="{{ assetOrDefault('assets/js/dashboards-analytics.js') }}"></script>
+
+    <!-- Place this tag before closing body tag for github widget button. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+</body>
+
+</html>
